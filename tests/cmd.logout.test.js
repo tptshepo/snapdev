7d686c03afd618e6d65540dad473e76d826f622d
Tshepo Mgaga <tshepo.mgaga@gmail.com>
2020-07-25 14:18:14 +0200

    Updated tests

---
+++ tests/cmd.logout.test.js
@@ -0,0 +1,22 @@
+const {
+  setupBeforeEach,
+  username,
+  password,
+  snapdev,
+} = require('./fixtures/setup');
+
+beforeEach(setupBeforeEach);
+
+test('snapdev logout success', async () => {
+  let result;
+  // login
+  result = await snapdev(`login --username ${username} --password ${password}`);
+  expect(result.code).toBe(0);
+  expect(result.stdout).toContain(`Logged in as: ${username}`);
+  expect(result.stdout).toContain(`Login Succeeded`);
+  
+  // logout
+  result = await snapdev(`logout`);
+  expect(result.code).toBe(0);
+  expect(result.stdout).toContain(`Removed login credentials`);
+});
